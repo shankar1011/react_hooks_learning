e84a31dd849330a17e27ac689d43da8c4b9d4df9
shankar1011 <shankarupadhyay1011@gmail.com>
2020-12-28 19:58:09 +0530

    added error handling using try catch

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -17,19 +17,22 @@ const Convert = ({ language, text }) => {
 
   useEffect(() => {
     const doTranslation = async () => {
-      const { data } = await axios.post(
-        'https://translation.googleapis.com/language/translate/v2',
-        {},
-        {
-          params: {
-            q: debouncedText,
-            target: language.value,
-            key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
-          },
-        }
-      );
-
-      setTranslated(data.data.translations[0].translatedText);
+      try {
+        const { data } = await axios.post(
+          'https://translation.googleapis.com/language/translate/v2',
+          {},
+          {
+            params: {
+              q: debouncedText,
+              target: language.value,
+              key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
+            },
+          }
+        );
+        setTranslated(data.data.translations[0].translatedText);
+      } catch(error) {
+        setTranslated("Oop's unable to translate :( ");
+      }
     };
 
     doTranslation();
