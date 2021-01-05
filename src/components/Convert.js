505cc732d08858a913b0e6ff1c2227cb91924dbb
shankar1011 <shankarupadhyay1011@gmail.com>
2021-01-05 15:12:14 +0530

    made changes to leverage useDebounce hook for debounce

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,36 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import useDebounce from "./utils/Debounce";
 
 const Convert = ({ language, text }) => {
-  const [translated, setTranslated] = useState('');
-  const [debouncedText, setDebouncedText] = useState(text);
+  const [translated, setTranslated] = useState("");
 
-  useEffect(() => {
-    const timerId = setTimeout(() => {
-      setDebouncedText(text);
-    }, 500);
-
-    return () => {
-      clearTimeout(timerId);
-    };
-  }, [text]);
+  const debouncedText = useDebounce(text, 500);
 
   useEffect(() => {
     const doTranslation = async () => {
       try {
         const { data } = await axios.post(
-          'https://translation.googleapis.com/language/translate/v2',
+          "https://translation.googleapis.com/language/translate/v2",
           {},
           {
             params: {
               q: debouncedText,
               target: language.value,
-              key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
+              key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
             },
           }
         );
         setTranslated(data.data.translations[0].translatedText);
-      } catch(error) {
+      } catch (error) {
         setTranslated("Oop's unable to translate :( ");
       }
     };
@@ -40,7 +33,7 @@ const Convert = ({ language, text }) => {
 
   return (
     <div>
-      <h1 className="ui header">{translated}</h1>
+      <h1 className='ui header'>{translated}</h1>
     </div>
   );
 };
